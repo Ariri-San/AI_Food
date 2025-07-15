5370497874ea3e0df3ce8eb3966c7937d2781927
Shayan <shayanghodos@gmail.com>
2025-07-15 20:09:44 +0330

    debug for deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-TEMPLATES[0]['DIRS'] += [os.path.join(BASE_DIR, 'frontend')]
+TEMPLATES[0]['DIRS'] += [os.path.join(BASE_DIR, 'frontend/build')]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
 MEDIA_URL = '/media/'
