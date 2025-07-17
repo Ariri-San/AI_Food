98b7a21cc243f8fb56cf94b1807e853687e36b5e
Shayan <shayanghodos@gmail.com>
2025-07-17 14:20:32 +0330

    add CSRF trust

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,6 +138,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
