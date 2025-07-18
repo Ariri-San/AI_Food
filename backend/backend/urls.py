8f426d103dc0fca6ef3db2af37bcd0e84559fc18
Shayan <shayanghodos@gmail.com>
2025-07-18 15:46:55 +0330

    add static root

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,9 +22,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("frontend.urls")),
     path("api/food/", include("ai_api.urls")),
+    path("", include("frontend.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
